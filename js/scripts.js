260d87cda1652110889e63c4d37c73c109eab833
VlaDSerga <65920800+VlaDSerga@users.noreply.github.com>
2025-09-19 22:45:53 +0300

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -146,6 +146,52 @@ $(() => {
 			},
 		})
 	}
+
+	if ($('.portfolio__slider').length) {
+		new Swiper('.portfolio__slider', {
+			loop: false,
+			watchSlidesProgress: true,
+			watchOverflow: true,
+			spaceBetween: 16,
+			slidesPerView: 3,
+			preloadImages: false,
+			lazy: {
+				loadPrevNext: true,
+				elementClass: 'lazyload',
+				enabled: true,
+				loadedClass: 'loaded',
+				checkInView: true,
+				loadOnTransitionStart: true
+			},
+			navigation: {
+				nextEl: '.slider-button-next',
+				prevEl: '.slider-button-prev'
+			},
+			breakpoints: {
+				'320': {
+					spaceBetween: 16,
+					slidesPerView: 3
+				},
+				'480': {
+					spaceBetween: 16,
+					slidesPerView: 3
+				},
+				'768': {
+					spaceBetween: 16,
+					slidesPerView: 3
+				},
+				'1024': {
+					spaceBetween: 16,
+					slidesPerView: 4,
+				}
+			},
+			on: {
+				init: function (swiper) {
+					$(swiper.el).find('.swiper-wrapper').wrap('<div class="swiper-overflow"></div>')
+				},
+			}
+		})
+	}
 	
 	// Добавление/Удаление (избранное)
 	$('body').on('click', '.favorite-js', function (e) {
